e8a743a1f208afe13b26d0ca45ae6f678d9462d6
Jeeva <jeevasdckap@gmail.com>
2023-02-27 22:14:14 +0530

    EMI calci app

---
+++ EMI Calculator.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector("#submit")
+let amt = document.querySelector("#amt")
+let interest = document.querySelector("#interest")
+let month = document.querySelector("#month")
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    let principleAmount = amt.value
+    let interests = interest.value
+    let months = month.value
+
+
+
+    let finalR = interests / (12 * 100)
+
+    let result = (principleAmount * finalR)
+    let result1 = (Math.pow(1 + finalR, months))
+    let result2 = result1 - 1
+
+    let FinelResult = result * result1
+
+    let Answer = FinelResult / result2
+
+    document.querySelector("#answer").innerText = Answer
+})
+
