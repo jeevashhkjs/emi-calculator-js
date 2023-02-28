5919eacb2ebd75f1a57209e5a4db2720c134366a
jeevashhkjs <jeevasdckap@gmail.com>
2023-02-28 16:43:08 +0530

    my add

--- EMI Calculator.js
+++ EMI Calculator.js
@@ -3,7 +3,6 @@ let amt = document.querySelector("#amt")
 let interest = document.querySelector("#interest")
 let month = document.querySelector("#month")
 let value = document.querySelector("#yandm")
-console.log(value)
 
 btn.addEventListener("click", (e) => {
 
@@ -14,11 +13,10 @@ btn.addEventListener("click", (e) => {
 
     if(value.value == "month"){
         monthWork(principleAmounts,interestss,monthss)
-    }else{
+     }
+    else if(value.value == "year"){
         YearWork(principleAmounts,interestss,monthss)
     }
-
-
 
 })
 
@@ -36,14 +34,16 @@ function monthWork(principleAmount,interests,months){
     
         let Answer = FinelResult / result2
 
-        let fixedNum = parseFloat(Answer).toFixed( 2 );
+        // let fixedNum = parseFloat(Answer).toFixed( 2 );
 
         let TotalAmount = months * Answer
         let interestsss = TotalAmount - principleAmount
+        console.log(TotalAmount)
     
-        document.querySelector("#answer").innerHTML = `Your EMI Amount is:<h5>${fixedNum}</h5>`
+        document.querySelector("#answer").innerHTML = `Your EMI Amount is:<h5>${Math.floor(Answer)}</h5>`
         document.querySelector("#amount").innerHTML = `Your Total Payable is:<h5>${Math.floor(TotalAmount)}</h5>`
         document.querySelector("#interst").innerHTML = `Your interest is:<h5>${Math.floor(interestsss)}</h5>`
+
     }else{
         alert("Please enter value")
     }
@@ -51,24 +51,31 @@ function monthWork(principleAmount,interests,months){
 
 
 function YearWork(principleAmount,interests,months){
-    let month = months * 12
-    if(amt.value != "" && interest.value != "" && value.value != ""){
+    let monthss = months * 12
+    if(amt.value != "" && interest.value != "" && month.value != "" && value.value){
         let finalR = interests / (12 * 100)
 
+
         let result = (principleAmount * finalR)
-        let result1 = (Math.pow(1 + finalR, month))
+    
+        let result1 = (Math.pow(1 + finalR, monthss))
         let result2 = result1 - 1
     
         let FinelResult = result * result1
     
         let Answer = FinelResult / result2
 
-        let fixedNum = parseFloat(Answer).toFixed( 2 );
+        // let fixedNum = parseFloat(Answer).toFixed( 2 );
+
+        let TotalAmount = months * Answer
+        let interestsss = TotalAmount - principleAmount
     
-        document.querySelector("#answer").innerText = `Your EMI Amount is:${fixedNum}`
-    }else{
-        alert("Please enter value")
-    }
+        document.querySelector("#answer").innerHTML = `Your EMI Amount is:<h5>${Math.floor(Answer)}</h5>`
+        document.querySelector("#amount").innerHTML = `Your Total Payable is:<h5>${Math.floor(TotalAmount)}</h5>`
+        document.querySelector("#interst").innerHTML = `Your interest is:<h5>${Math.floor(interestsss)}</h5>`
+}else{
+    alert("Please enter value")
+}
 }
 
 
