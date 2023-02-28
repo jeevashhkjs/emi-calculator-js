6f268f20c37cd621cb5990672ff90dd133f700b4
jeevashhkjs <jeevasdckap@gmail.com>
2023-02-28 15:39:24 +0530

    full version

--- EMI Calculator.js
+++ EMI Calculator.js
@@ -26,7 +26,9 @@ function monthWork(principleAmount,interests,months){
     if(amt.value != "" && interest.value != "" && month.value != "" && value.value){
         let finalR = interests / (12 * 100)
 
+
         let result = (principleAmount * finalR)
+    
         let result1 = (Math.pow(1 + finalR, months))
         let result2 = result1 - 1
     
@@ -35,8 +37,13 @@ function monthWork(principleAmount,interests,months){
         let Answer = FinelResult / result2
 
         let fixedNum = parseFloat(Answer).toFixed( 2 );
+
+        let TotalAmount = months * Answer
+        let interestsss = TotalAmount - principleAmount
     
-        document.querySelector("#answer").innerText = `Your EMI Amount is:${fixedNum}`
+        document.querySelector("#answer").innerHTML = `Your EMI Amount is:<h5>${fixedNum}</h5>`
+        document.querySelector("#amount").innerHTML = `Your Total Payable is:<h5>${Math.floor(TotalAmount)}</h5>`
+        document.querySelector("#interst").innerHTML = `Your interest is:<h5>${Math.floor(interestsss)}</h5>`
     }else{
         alert("Please enter value")
     }
