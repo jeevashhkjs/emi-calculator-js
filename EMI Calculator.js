6951d2a2ea79eaa646548b7b36152416fdd55c13
Jeeva <jeevasdckap@gmail.com>
2023-03-04 21:30:01 +0530

    buttons all are worked except two buttons

--- EMI Calculator.js
+++ EMI Calculator.js
@@ -9,8 +9,11 @@ let year = document.querySelector("#year")
 let range = document.querySelectorAll('input[type="range"]')
 let inputs = document.querySelectorAll('input[type="text"]')
 
-for(let j=0;j<range.length;j++){
-    range[j].addEventListener("input",()=>{
+let rsbtn = document.querySelector("#rs")
+let interestbtn = document.querySelector("#inter")
+
+for (let j = 0; j < range.length; j++) {
+    range[j].addEventListener("input", () => {
         inputs[j].value = range[j].value
     })
 }
@@ -18,13 +21,27 @@ for(let j=0;j<range.length;j++){
 
 year.classList.add("colors")
 
-let inp = document.querySelector("#amt")
-inp.addEventListener("keyup", () => {
-  document.querySelector("#amtrange").value = inp.value
+// let inp = document.querySelector("#amt")
+// inp.addEventListener("keyup", () => {
+//   document.querySelector("#amtrange").value = inp.value
+// })
+
+rsbtn.addEventListener("click", (e) => {
+    let amouts = amt.value
+    let intrs = interest.value
+    let mnths = month.value
+    e.preventDefault()
+    checking(amouts, intrs, mnths)
+})
+interestbtn.addEventListener("click", (e) => {
+    let amouts = amt.value
+    let intrs = interest.value
+    let mnths = month.value
+    e.preventDefault()
+    checking(amouts, intrs, mnths)
 })
 
-
-for(let l=0;l<btn.length;l++){
+for (let l = 0; l < btn.length; l++) {
     btn[l].addEventListener("click", (e) => {
 
         e.preventDefault()
@@ -32,15 +49,15 @@ for(let l=0;l<btn.length;l++){
         let interestss = interest.value
         let monthss = month.value
 
-        checking(principleAmounts,interestss,monthss)
-    
+        checking(principleAmounts, interestss, monthss)
+
     })
 
 }
 
-year.addEventListener("click",(e)=>{
+year.addEventListener("click", (e) => {
     e.preventDefault()
-    if(month.value > 1){
+    if (month.value > 1) {
         month.value = month.value / 12
     }
 
@@ -48,64 +65,65 @@ year.addEventListener("click",(e)=>{
     mnbtn.classList.remove("colors")
 })
 
-mnbtn.addEventListener("click",(e)=>{
+mnbtn.addEventListener("click", (e) => {
     e.preventDefault()
-    month.value = month.value * 12
-
+    if (mnbtn.classList.contains("colors") == false) {
+        month.value = month.value * 12
+    }
     year.classList.remove("colors")
     mnbtn.classList.add("colors")
 })
 
-function checking(principleAmounts,interestss,monthss){
+function checking(principleAmounts, interestss, monthss) {
 
     let year = document.querySelector("#year")
-    if(year.classList.contains("colors")){
-     YearWork(principleAmounts,interestss,monthss)
-    }else{
-     monthWork(principleAmounts,interestss,monthss)
+    if (year.classList.contains("colors")) {
+        YearWork(principleAmounts, interestss, monthss)
+    } else {
+        monthWork(principleAmounts, interestss, monthss)
     }
 
-    mnbtn.addEventListener("click",()=>{
+    mnbtn.addEventListener("click", () => {
         let year = document.querySelector("#year")
-        if(year.classList.contains("colors")){
-         YearWork(principleAmounts,interestss,monthss)
-        }else{
-         monthWork(principleAmounts,interestss,monthss)
+        if (year.classList.contains("colors") == true) {
+            YearWork(principleAmounts, interestss, monthss)
+        } else {
+            monthWork(principleAmounts, interestss, monthss)
         }
     })
 
 }
 
 
-function monthWork(principleAmount,interests,months){
-    
-    if(amt.value != "" && interest.value != "" && month.value != ""){
+function monthWork(principleAmount, interests, months) {
 
-        if(amt.value == 0 || interest.value == 0 || month.value == 0){
+    if (amt.value != "" && interest.value != "" && month.value != "") {
+
+        if (amt.value == 0 || interest.value == 0 || month.value == 0) {
             alert("Please put greater than 1")
         }
-        else{
+        else {
             let finalR = interests / (12 * 100)
 
             let result = (principleAmount * finalR)
-        
+
             let result1 = (Math.pow(1 + finalR, months))
             let result2 = result1 - 1
-        
+
             let FinelResult = result * result1
-        
+
             let Answer = FinelResult / result2
-    
+
             let TotalAmount = months * Answer
             let interestsss = TotalAmount - principleAmount
             console.log(interestsss)
-        
+
             document.querySelector("#answer").innerHTML = "₹" + Math.floor(Number(Answer))
             document.querySelector("#amount").innerHTML = "₹" + Math.floor(Number(TotalAmount))
             document.querySelector("#interst").innerHTML = "₹" + Math.floor(Number(interestsss))
         }
     }
-    else{
+    else {
         alert("Please enter value")
     }
 }
@@ -113,29 +131,31 @@ function monthWork(principleAmount,interests,months){
 
 
 
-function YearWork(principleAmount,interests,months){
-let monthss = months * 12
-if(amt.value != "" && interest.value != "" && month.value != ""){
-    let finalR = interests / (12 * 100)
+function YearWork(principleAmount, interests, months) {
+    let monthss = months * 12
+    if (amt.value != "" && interest.value != "" && month.value != "") {
+        let finalR = interests / (12 * 100)
 
-    let result = (principleAmount * finalR)
+        let result = (principleAmount * finalR)
 
-    let result1 = (Math.pow(1 + finalR, monthss))
-    let result2 = result1 - 1
+        let result1 = (Math.pow(1 + finalR, monthss))
+        let result2 = result1 - 1
 
-    let FinelResult = result * result1
+        let FinelResult = result * result1
 
-    let Answer = FinelResult / result2
+        let Answer = FinelResult / result2
 
-    let TotalAmount = months * Answer
-    let interestsss = TotalAmount - principleAmount
+        let TotalAmount = monthss * Answer
+        let interestsss = TotalAmount - principleAmount
+        console.log(TotalAmount)
+        console.log(interestsss)
 
-    document.querySelector("#answer").innerHTML = "₹" + Math.floor(Number(Answer))
-    document.querySelector("#amount").innerHTML = "₹" + Math.floor(Number(TotalAmount))
-    document.querySelector("#interst").innerHTML = "₹" + Math.floor(Number(interestsss))
-}else{
-alert("Please enter value")
-}
+        document.querySelector("#answer").innerHTML = "₹" + Math.floor(Number(Answer))
+        document.querySelector("#amount").innerHTML = "₹" + Math.floor(Number(TotalAmount))
+        document.querySelector("#interst").innerHTML = "₹" + Math.floor(Number(interestsss))
+    } else {
+        alert("Please enter value")
+    }
 }
 
 
