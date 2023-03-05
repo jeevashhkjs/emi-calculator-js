77d2268564c62b1bb771f08e533b5b025d8dfc43
Jeeva <jeevasdckap@gmail.com>
2023-03-05 17:37:53 +0530

    All bugs fixed

--- EMI Calculator.js
+++ EMI Calculator.js
@@ -1,30 +1,27 @@
 let btn = document.querySelectorAll('input[type="range"]')
 let amt = document.querySelector("#amt")
+
 let interest = document.querySelector("#interest")
 let month = document.querySelector("#month")
 let mnbtn = document.querySelector("#mnth")
 let year = document.querySelector("#year")
 
-
 let range = document.querySelectorAll('input[type="range"]')
-let inputs = document.querySelectorAll('input[type="text"]')
+let inputs = document.querySelectorAll('input[type="number"]')
 
 let rsbtn = document.querySelector("#rs")
 let interestbtn = document.querySelector("#inter")
+let notification = document.querySelector("#notify")
 
 for (let j = 0; j < range.length; j++) {
     range[j].addEventListener("input", () => {
         inputs[j].value = range[j].value
+        inputs[j].value = inputs[j].value.toLocaleString()
     })
 }
 
 
 year.classList.add("colors")
-
-// let inp = document.querySelector("#amt")
-// inp.addEventListener("keyup", () => {
-//   document.querySelector("#amtrange").value = inp.value
-// })
 
 rsbtn.addEventListener("click", (e) => {
     let amouts = amt.value
@@ -61,6 +58,12 @@ year.addEventListener("click", (e) => {
         month.value = month.value / 12
     }
 
+    let amouts = amt.value
+    let intrs = interest.value
+    let mnths = month.value
+    e.preventDefault()
+    checking(amouts, intrs, mnths)
+
     year.classList.add("colors")
     mnbtn.classList.remove("colors")
 })
@@ -70,6 +73,13 @@ mnbtn.addEventListener("click", (e) => {
     if (mnbtn.classList.contains("colors") == false) {
         month.value = month.value * 12
     }
+
+    let amouts = amt.value
+    let intrs = interest.value
+    let mnths = month.value
+    e.preventDefault()
+    checking(amouts, intrs, mnths)
+
     year.classList.remove("colors")
     mnbtn.classList.add("colors")
 })
@@ -94,13 +104,15 @@ function checking(principleAmounts, interestss, monthss) {
 
 }
 
-
 function monthWork(principleAmount, interests, months) {
 
     if (amt.value != "" && interest.value != "" && month.value != "") {
 
         if (amt.value == 0 || interest.value == 0 || month.value == 0) {
-            alert("Please put greater than 1")
+            notification.style.display = "block"
+            setInterval(() => {
+                notification.style.display = "none"
+            },1000);
         }
         else {
             let finalR = interests / (12 * 100)
@@ -116,11 +128,15 @@ function monthWork(principleAmount, interests, months) {
 
             let TotalAmount = months * Answer
             let interestsss = TotalAmount - principleAmount
-            console.log(interestsss)
 
-            document.querySelector("#answer").innerHTML = "₹" + Math.floor(Number(Answer))
-            document.querySelector("#amount").innerHTML = "₹" + Math.floor(Number(TotalAmount))
-            document.querySelector("#interst").innerHTML = "₹" + Math.floor(Number(interestsss))
+            let finalAnswer = Answer.toFixed(2)
+            let finalInterest = interestsss.toFixed(2)
+            let resTotalamount = TotalAmount.toFixed(2)
+
+
+            document.querySelector("#answer").innerHTML = "₹" + finalAnswer.toLocaleString()
+            document.querySelector("#amount").innerHTML = "₹" + resTotalamount.toLocaleString()
+            document.querySelector("#interst").innerHTML = "₹" + finalInterest.toLocaleString()
         }
     }
     else {
@@ -134,25 +150,38 @@ function monthWork(principleAmount, interests, months) {
 function YearWork(principleAmount, interests, months) {
     let monthss = months * 12
     if (amt.value != "" && interest.value != "" && month.value != "") {
-        let finalR = interests / (12 * 100)
 
-        let result = (principleAmount * finalR)
+        if (amt.value == 0 || interest.value == 0 || month.value == 0) {
+            notification.style.display = "block"
+            setInterval(() => {
+                notification.style.display = "none"
+            },1000);
+        }
+        else {
+            let finalR = interests / (12 * 100)
 
-        let result1 = (Math.pow(1 + finalR, monthss))
-        let result2 = result1 - 1
+            let result = (principleAmount * finalR)
 
-        let FinelResult = result * result1
+            let result1 = (Math.pow(1 + finalR, monthss))
+            let result2 = result1 - 1
 
-        let Answer = FinelResult / result2
+            let FinelResult = result * result1
 
-        let TotalAmount = monthss * Answer
-        let interestsss = TotalAmount - principleAmount
-        console.log(TotalAmount)
-        console.log(interestsss)
+            let Answer = FinelResult / result2
 
-        document.querySelector("#answer").innerHTML = "₹" + Math.floor(Number(Answer))
-        document.querySelector("#amount").innerHTML = "₹" + Math.floor(Number(TotalAmount))
-        document.querySelector("#interst").innerHTML = "₹" + Math.floor(Number(interestsss))
+            let TotalAmount = monthss * Answer
+            let interestsss = TotalAmount - principleAmount
+
+            let finalAnswer = Answer
+            let finalInterest = interestsss
+            let resTotalamount = TotalAmount
+
+
+            document.querySelector("#answer").innerHTML = "₹" + Math.round(finalAnswer).toLocaleString()
+            document.querySelector("#amount").innerHTML = "₹" + Math.round(resTotalamount).toLocaleString()
+            document.querySelector("#interst").innerHTML = "₹" + Math.round(finalInterest).toLocaleString()
+        }
+
     } else {
         alert("Please enter value")
     }
